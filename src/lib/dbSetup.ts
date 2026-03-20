2a0a99902343bd848b52c12fcfbf257fa878f1c7
AkshatJain <jainakshat_02@protonmail.com>
2026-03-20 13:02:35 +0530

    project setup

---
+++ src/lib/dbSetup.ts
@@ -0,0 +1,26 @@
+import mongoose from "mongoose";
+
+type ConnectionObject = {
+  isConnected?: number;
+};
+
+const connection: ConnectionObject = {};
+
+async function connect(): Promise<void> {
+  if (connection.isConnected) {
+    console.log("Already connected");
+    return;
+  }
+
+  try {
+    console.log("Connecting to MongoDB");
+    const db = await mongoose.connect(process.env.MONGO_URI!);
+    connection.isConnected = db.connections[0].readyState;
+    console.log("MongoDB connected successfully");
+  } catch (error: unknown) {
+    console.log("Something went wrong", (error as Error).message);
+    process.exit(1);
+  }
+}
+
+export default connect;
